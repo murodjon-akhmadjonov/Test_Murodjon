ad120023bb9925ecfe4ee8062ffce135f32ab00a
murodjon-akhmadjonov <ahmadjonovmurodjon28@gmail.com>
2025-10-06 16:56:24 +0500

    added print

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello wolrd')
+# polymorphism
+# incapsulation in OOP 
+
+class User:
+    def __init__(self, name ,age):
+        self.name = name
+        self.age = age
+
+
+    def show_info(self):
+        print(f'Name: {self.name}, Age: {self.age}')
+
+user = User("john",30)
+user.show_info()
+
+
+class Admin(User):
+    def __init__(self, name, age, role):
+        super().__init__(name, age)
+        self.role = role
+
+    def show_info(self):
+        super().show_info()
+        print(f"Role: {self.role}")
+
+admin = Admin(f"John", 30 , "Admin")
+admin.show_info()
